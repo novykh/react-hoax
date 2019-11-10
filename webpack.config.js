ea56edd704e58c12923dfd3a29f250673ded5b7f
Johnny Klironomos <ioannis.klironomos@gmail.com>
2019-11-10 17:22:26 +0200

    Add aliases for react packages.

--- webpack.config.js
+++ webpack.config.js
@@ -34,5 +34,11 @@ module.exports = {
         }
       }
     ]
+  },
+  resolve: {
+    alias: {
+      react: path.resolve(__dirname, "./node_modules/react"),
+      "react-dom": path.resolve(__dirname, "./node_modules/react-dom")
+    }
   }
 };
