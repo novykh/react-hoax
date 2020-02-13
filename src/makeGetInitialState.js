2507af1339e6439c8986f77c08b41b06abc6f702
Johnny Klironomos <ioannis.klironomos@gmail.com>
2020-02-13 13:50:09 +0200

    Fixed initialState.

--- src/makeGetInitialState.js
+++ src/makeGetInitialState.js
@@ -1,8 +1,5 @@
 export default ({ getInitialState, defaultInitialState }) => {
-  if (getInitialState && typeof getInitialState !== "function") {
-    console.error(
-      "Hoax needs a function to return the initial state object - empty object will be used instead"
-    );
+  if (!getInitialState || typeof getInitialState !== "function") {
     return () => defaultInitialState;
   }
 
