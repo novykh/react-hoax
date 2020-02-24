a20900693cd96ae8cb276f25f90f924593b1e2e6
Johnny Klironomos <ioannis.klironomos@gmail.com>
2020-02-24 11:59:50 +0200

    Fixed remove resource issue.

--- src/collection/makeReducer.js
+++ src/collection/makeReducer.js
@@ -76,13 +76,13 @@ export default ({
   };
 
   const removeResource = (state, resourceId) => {
-    const byId = delete state.byId[resourceId];
+    delete state.byId[resourceId];
     const ids = state.ids.filter(id => id !== resourceId);
 
     return {
       ...state,
       byId: {
-        ...byId
+        ...state.byId
       },
       ids
     };
